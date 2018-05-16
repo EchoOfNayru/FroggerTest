7abd0f3eb7776bed09c10bb34e2846ef60a731d9
Evan Knebel <s179060@students.aie.edu.au>
2018-05-16 12:28:09 -0700

    Player move very nearly complete. Movement forward and back could be smoother.

--- Assets/Evan/Scripts/Player/PlayerMove.cs
+++ Assets/Evan/Scripts/Player/PlayerMove.cs
@@ -8,17 +8,20 @@ public class PlayerMove : MonoBehaviour
     public float horizontalHopSpeed;
     public float moveTimerMax;
     public float moveTimer;
+    public float autoMoveTimerMax;
+    public float autoMoveTimer;
 
 	// Use this for initialization
 	void Start ()
     {
         moveTimer = moveTimerMax;
+        autoMoveTimer = autoMoveTimerMax;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        //Set layers vertical, variable movement horizontal
+//Set layers vertical, variable movement horizontal
         if (Input.GetKey(KeyCode.RightArrow))
         {
             if(moveTimer > 0)
@@ -26,12 +29,22 @@ public class PlayerMove : MonoBehaviour
                 gameObject.transform.Translate(horizontalHopSpeed * Time.deltaTime, 0, 0);
                 moveTimer -= Time.deltaTime;
             }
+            else if (moveTimer <= 0)
+            {
+                autoMoveTimer -= Time.deltaTime;
+                if (autoMoveTimer <= 0)
+                {
+                    moveTimer = moveTimerMax;
+                    autoMoveTimer = autoMoveTimerMax;
+                }
+            }
         }
         if (Input.GetKeyUp(KeyCode.RightArrow))
         {
             if (!Input.anyKey)
             {
                 moveTimer = moveTimerMax;
+                autoMoveTimer = autoMoveTimerMax;
             }
         }
 
@@ -42,15 +55,26 @@ public class PlayerMove : MonoBehaviour
                 gameObject.transform.Translate(-horizontalHopSpeed * Time.deltaTime, 0, 0);
                 moveTimer -= Time.deltaTime;
             }
+            else if (moveTimer <= 0)
+            {
+                autoMoveTimer -= Time.deltaTime;
+                if (autoMoveTimer <= 0)
+                {
+                    moveTimer = moveTimerMax;
+                    autoMoveTimer = autoMoveTimerMax;
+                }
+            }
         }
         if (Input.GetKeyUp(KeyCode.LeftArrow))
         {
             if (!Input.anyKey)
             {
                 moveTimer = moveTimerMax;
+                autoMoveTimer = autoMoveTimerMax;
             }
         }
 
+        //Layer movement
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             gameObject.transform.Translate(0, 0, gridMoveDistance);

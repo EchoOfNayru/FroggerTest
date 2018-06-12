3cd42bfac0f159f314f6ff134c013d2383682efe
Zach Tucker <ZachTucker385@gmail.com>
2018-06-11 18:01:26 -0700

    Level 1 finished

--- Assets/Zach/Scripts/RowScript.cs
+++ Assets/Zach/Scripts/RowScript.cs
@@ -3,9 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RowScript : MonoBehaviour {
-
-    public GameObject thingToSpawn;
-    public bool direction; // true - left, false - right
+    
     public int type;   // 0 = safe, 1 = danger, 2 = water
 
     void Start()
